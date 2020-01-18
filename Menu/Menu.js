f62c3e0379ae0f346dad6e8130f78c4415c9f138
Brennon Nuckols <47544816+brennuck@users.noreply.github.com>
2020-01-18 16:03:14 -0700

    Revert "New"

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -33,34 +33,3 @@ let menuItems = [
   Step 6: add the menu component to the DOM.
   
 */
-
-function menuComponent(array) {
-  // create new elements
-  const menuDiv = document.createElement('div');
-  const listParent = document.createElement('ul');
-
-  // add classes to elements
-  menuDiv.classList.add('menu');
-
-  // Step 2: Inside this function, iterate over the array creating a list item <li> element for each item in the array. Add those items to the <ul>
-  menuItems.forEach(data => {
-    let links = document.createElement('li');
-    links.textContent = data;
-    listParent.append(links);
-  });
-  menuDiv.append(listParent);
-
-  // Step 4: add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on the menu (your div with a 'menu' class).
-  const button = document.querySelector('.menu-button');
-  button.addEventListener('click', () => {
-    menuDiv.classList.toggle('menu--open');
-  });
-
-  // Step 5: return the menu component.
-  return menuDiv;
-}
-
-// Step 6: add the menu component to the DOM.
-menuComponent(menuItems);
-const hamburgerHeader = document.querySelector('.header');
-hamburgerHeader.append(menuComponent(menuItems));
